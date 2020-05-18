1fa0054388c85476362233b39c25ea2de40550ce
Etienne Pauthenet <etienne.pauthenet@gmail.com>
2020-05-18 15:04:16 +0200

    Update temp name

--- Animate_MEOP.R
+++ Animate_MEOP.R
@@ -7,7 +7,7 @@ path = "some_empty_folder"
 setwd(path)
 
 for(t in 1:length(lon)){
-  png(paste(t,".png",sep = ""), width=8,height=8, units="in", res=200)  #save figure as a png
+  png(paste(path1,t,".png",sep = ""), width=8,height=8, units="in", res=200)
   par(mfrow = c(2,2))
   plot(lon,lat,las = 1,col = 'grey',pch = 20,main = Time[t],typ = 'l',asp = 1
     ,xlim = c(min(lon)-2,max(lon)+2), ylim = c(min(lat)-2,max(lat)+2))
@@ -21,7 +21,7 @@ for(t in 1:length(lon)){
   TS.ctd = oce::as.ctd(Sal[,t],Temp[,t],prof)
   oce::plotTS(TS.ctd,las = 1,typ = 'l',xlab = "Salinity",ylab = "Temperature",col = 2,lwd = 2,add = T,inSitu = T)
   #
-  plot(Temp,Pres,las = 1,typ = 'l',ylim = c(max(prof),0),xlim = range(Temp,na.rm = T),xlab = "Salinity",ylab = "Depth",col = 'grey')
+  plot(Temp,Pres,las = 1,typ = 'l',ylim = c(max(prof),0),xlim = range(Temp,na.rm = T),xlab = "Temperature",ylab = "Depth",col = 'grey')
   lines(Temp[,t],prof,col = 2,lwd = 2)
   grid()
   #
@@ -32,8 +32,8 @@ for(t in 1:length(lon)){
 }
 
 #Export in a gif
-png_lf = list.files(path, pattern = ".*png$")
-png_lf = gtools::mixedsort(sort(png_lf))  #sort in the right order
-gifski::gifski(png_lf, gif_file = "animation.gif", width = 800, height = 800, delay = .2)
+png_lf = list.files(path1, pattern = ".*png$")
+png_lf = paste(path1,gtools::mixedsort(sort(png_lf)),sep = "")
+gifski::gifski(png_lf, gif_file = paste(path2,substr(lf[n],1,13),".gif",sep = ""), width = 800, height = 800, delay = .1)
 unlink(png_lf)  #Delete the png files
 
